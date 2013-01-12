045f87e6008004ee7c84678d80c1fb472ec2896c
Ubuntu <ubuntu@ip-10-244-175-165.us-west-2.compute.internal>
2013-01-12 16:05:41 +0000

    getting list of unique repos

--- uniques.js
+++ uniques.js
@@ -1,8 +1,27 @@
 
-var db = require('./lib/db'),
-    redis = require('redis');
+var mongo = require('mongodb'),
+    redis = require('redis').createClient(),
+    format = require('util').format;
 
 
-db.repos(function (err, repos) {
-    console.log(repos.count());
+mongo.Db.connect(
+    format("mongodb://%s:%s/github-nightowls?w=1", 'localhost', 27017),
+    function(err, db) {
+        
+        var repos = db.collection('repos');
+
+        repos.find().each(function (err, repo) {
+            if (err) {
+                console.log(err);
+                return;
+            }
+
+            var name = format('%s/%s', repo.owner, repo.name);
+            
+            redis.sadd("repositories", name);
+            redis.incr("N_repos");
+
+            console.log(name+" added to set");
+        });
+              
 });
